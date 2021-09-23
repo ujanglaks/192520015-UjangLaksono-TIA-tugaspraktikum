4a874405e1b18a6dac583ee67a856f9b71776ee1
ujanglaks <91252263+ujanglaks@users.noreply.github.com>
2021-09-23 18:47:18 +0700

    Add files via upload

---
+++ latihan.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "Ujang Laksono";
+$nim = 192520015;
+$alamat = "Krajan RT 02 / RW 01, Redin, Gebang, Purworejo";
+$alamat_email = "ujanglaks@gmail.com";
+$minat = "Jaringan";
+$universitas = "Universitas Muhammadiyah Purworejo";
+
+echo "<h2>Data Diri</h2>";
+echo "<br>";
+echo "Nama : ".$nama;
+echo "<br>";
+echo "NIM : ".$nim;
+echo "<br>";
+echo "Alamat : ".$alamat;
+echo "<br>";
+echo "Alamat Email : ".$alamat_email;
+echo "<br>";
+echo "Bidang Minat : ".$minat;
+echo "<br>";
+echo "Universitas : ".$universitas;
+echo "<br>";
+
+?>
